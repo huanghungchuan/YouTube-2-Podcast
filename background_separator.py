de3db2ac196759d091ba128c086f61d662a269c2
Hung-Chuan Huang <huanghungchuan@gmail.com>
2021-03-11 20:26:00 +0000

    Create a new file that will separate the background noise and the vocal signals.

---
+++ background_separator.py
@@ -0,0 +1,15 @@
+import argparse
+import sys
+
+from spleeter.separator import Separator
+
+def main(args):
+    parser = argparse.ArgumentParser(description="Do something.")
+    parser.add_argument("-input_path", "--input_path", type=str, default='', required=False)
+    parser.add_argument("-output_directory", "--output_directory", type=str, default='', required=False)
+    separator = Separator('spleeter:2stems')
+    args = parser.parse_args(args)
+    separator.separate_to_file(args.input_path, args.output_directory)
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
